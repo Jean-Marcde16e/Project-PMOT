140b4ffe7591078de89e79f9aa7ef96081ceaa3f
Pascal Wiersma <info@pascalservices.nl>
2024-05-15 15:22:08 +0200

    chore: Add react-loading-skeleton package and update productCard component

--- components/ui/cards/productCard.tsx
+++ components/ui/cards/productCard.tsx
@@ -13,13 +13,21 @@ import { ShoppingCart } from "lucide-react";
 function ProductCard() {
     return (
         <div>
+            <div className="py-4">
+                <h1 className="text-3xl font-bold">
+                    Webshop
+                </h1>
+                <p>
+                    Bekijk hier alle producten in onze webshop.
+                </p>
+            </div>
             <Card className="w-80">
              <img src="https://via.placeholder.com/200" alt="Product" className="w-full" />
                 <CardHeader>
                     <CardTitle>Product</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <p>Card Content</p>
+                    <CardDescription>Product description</CardDescription>
                 </CardContent>
                 <CardFooter>
                     <div className="flex justify-between items-center gap-x-2">
