079b0e0c1d63acb5bed464b5685449b0463d42c4
Silvaire <sbdsikkema@st.noorderpoort.nl>
2024-05-17 08:55:49 +0200

    Reponsive fixed Footer, ContactForm, ero, Reviews

--- components/index/ContactForm.tsx
+++ components/index/ContactForm.tsx
@@ -5,28 +5,28 @@ import { MapPin } from "lucide-react";
 
 function ContactForm() {
     return (
-        <section className="h-fit bg-neutral-900 grid md:grid-cols-2 lg:gap-12 lg:my-20 my-10 gap-8  px-4 py-8 lg:p-16 rounded-md">
+        <section className="h-fit bg-white grid md:grid-cols-2 lg:gap-12 lg:my-20 my-10 gap-8 w-full px-4 py-8 lg:p-16 rounded-md">
 
             <div className="flex-col justify-start items-start gap-6 inline-flex my-auto px-4 lg:px-16">
 
                 <div className="rounded-2xl justify-start items-center gap-4 inline-flex">
                     <Mail color="#BC201B" />
-                    <p className="text-white text-xl font-medium font-roboto underline">info@pmot.nl</p>
+                    <p className="text-black text-xl font-medium font-roboto underline">info@pmot.nl</p>
                 </div>
 
                 <div className="rounded-2xl border -ml-4 md:-ml-6 border-secondary justify-start items-center gap-4 px-4 py-1 md:px-6 md:py-3 flex">
                     <Phone color="#BC201B" />
-                    <p className="text-white text-xl font-medium font-roboto"> +31 (0)597 591596</p>
+                    <p className="text-blacktext-xl font-medium font-roboto"> +31 (0)597 591596</p>
                 </div>
 
                 <div className="rounded-2xl justify-start items-center gap-4 inline-flex">
                     <MapPin color="#BC201B" />
-                    <p className="text-white text-xl font-medium font-['Poppins']">Torenstraat 13 9679 BN Scheemda</p>
+                    <p className="text-blacktext-xl font-medium font-['Poppins']">Torenstraat 13 9679 BN Scheemda</p>
                 </div>
 
             </div>
 
-            <form className="px-8 lg:px-16 bg-white flex-col justify-center items-start rounded-md flex">
+            <form className="px-8 lg:px-16 flex-col justify-center items-start rounded-md flex">
 
                 <div className="w-full h-full py-12 lg:py-16 flex flex-col gap-4">
                     <h1 className="text-neutral-900 text-2xl sm:text-3xl md:text-5xl font-semibold font-['Roboto']">Neem contact op!</h1>
